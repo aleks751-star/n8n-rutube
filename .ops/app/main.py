c68bca83b176efa557c2d1010c6b4986faf1889b
aleks751-star <aleks751ivan@gmail.com>
2025-08-11 01:07:10 +0300

    Delete .ops/app/main.py

--- .ops/app/main.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import socket
-
-def main():
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[app] started at {now} on {socket.gethostname()}")
-
-if __name__ == "__main__":
-    main()
